230a4564250eb99c328df42f14ddc87426decf58
Andy Nguyen <andy@mahalo.com>
2012-10-18 10:56:44 -0700

    added models for accounts and videos

--- herpderp/settings.py
+++ herpderp/settings.py
@@ -1,4 +1,4 @@
-# Django settings for herpderp project.
+import os
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -11,14 +11,17 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '/home/andy/code/heroku/herpderp.db',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
+
+PROJECT_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), '../'))
+SETTINGS_PATH = os.path.abspath(os.path.dirname(__file__))
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -119,7 +122,8 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-    
+    'accounts',
+    'videos',
 )
 
 # A sample logging configuration. The only tangible logging
@@ -151,7 +155,11 @@ LOGGING = {
     }
 }
 
-import dj_database_url
-DATABASES['default'] = dj_database_url.config()
+if SETTINGS_PATH != '/home/andy/code/heroku/herpderp/herpderp':
+    import dj_database_url
+    DATABASES['default'] = dj_database_url.config()
+
+# CONSTANTS
+URL_MAX_LENGTH = 2048
 
 
--- videos/models.py
+++ videos/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+import accounts.models as accounts_models
+
+class Video(models.Model):
+    url = models.URLField(max_length=settings.URL_MAX_LENGTH)
+    
+class Rating(models.Model):
+    video = models.ForeignKey(Video)
+    user = models.ForeignKey(accounts_models.User)
+    rating = models.IntegerField()
+
