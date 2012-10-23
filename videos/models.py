ffbbd25e1a50cf3697adb8e071e1bdb32e5e8e1a
Andy Nguyen <andy@mahalo.com>
2012-10-23 14:42:15 -0700

    added custom printing of video objects

--- videos/models.py
+++ videos/models.py
@@ -7,6 +7,9 @@ import accounts.models as accounts_models
 class Video(base_models.Base):
     yt_id = models.CharField(max_length=settings.YT_ID_MAX_LENGTH)
     
+    def __unicode__(self):
+        return self.yt_id;
+    
 class Rating(base_models.Base):
     video = models.ForeignKey(Video)
     user = models.ForeignKey(accounts_models.User)
