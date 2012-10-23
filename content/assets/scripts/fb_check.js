ef9ab5362445b49615cb6dc76b64f6a1360982e1
Andy Nguyen <andy@mahalo.com>
2012-10-23 15:38:10 -0700

    modified fb_login check

--- content/assets/scripts/fb_check.js
+++ content/assets/scripts/fb_check.js
@@ -1,4 +1,3 @@
-window.fbAsyncInit = function() {
     FB.init({
         appId      : '397851696951181', // App ID
         channelUrl : 'http://warm-ocean-6030.herokuapp.com/login/channel', // Channel File
@@ -19,14 +18,12 @@ window.fbAsyncInit = function() {
         }
     });
 
-};
-
 // Load the SDK Asynchronously
-(function(d){
-    var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
-    if (d.getElementById(id)) {return;}
-    js = d.createElement('script'); js.id = id; js.async = true;
-    js.src = "//connect.facebook.net/en_US/all.js";
-    ref.parentNode.insertBefore(js, ref);
-}(document));
+//(function(d){
+//    var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+//    if (d.getElementById(id)) {return;}
+//    js = d.createElement('script'); js.id = id; js.async = true;
+//    js.src = "//connect.facebook.net/en_US/all.js";
+//    ref.parentNode.insertBefore(js, ref);
+//}(document));
 
