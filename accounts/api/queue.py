1db57ffef8d11779973e0727aedaefa9b6403c04
Andy Nguyen <andy@mahalo.com>
2012-10-25 18:30:17 -0700

    added object returned by queue GET

--- accounts/api/queue.py
+++ accounts/api/queue.py
@@ -8,19 +8,22 @@ import videos.models as videos_models
 class QueueApi(base.RestView):
     
     def GET(self, request, fb_id, *args, **kwargs):
-        #try:
-        #    user = accounts_models.User.get(fb_id=user_id)
-        #except accounts_models.User.DoesNotExist:
-        #gh    return HttpResponseBadRequest('User ID invalid')
-        
-        uid = accounts_models.User.objects.get(fb_id=fb_id)
+
+        try:
+            uid = accounts_models.User.objects.get(fb_id=fb_id)
+        except accounts_models.User.DoesNotExist:
+            return HttpResponseBadRequest('Invalid fb_id')
         vid_ids = [v.video_id for v in 
                    videos_models.Rating.objects.filter(user_id=uid)]
         videos = list(videos_models.Video.objects.all())
         videos = filter(lambda x: x.id not in vid_ids, videos)
         
+        vids = []
+        for vid in videos:
+            vids.append({'yt_id': vid.yt_id, 'reward': vid.reward})
+        
         data = {
-            'vid_ids': [vid.yt_id for vid in videos],
+            'vids': vids,
             'host': request.get_host()
         }
         
