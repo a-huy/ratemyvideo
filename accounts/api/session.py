4fe99a26c1c3271b12ec283ada3163c9b3cafccc
Andy Nguyen <andy@mahalo.com>
2012-10-26 14:58:04 -0700

    added fb_id checking for session GET

--- accounts/api/session.py
+++ accounts/api/session.py
@@ -7,7 +7,7 @@ class SessionApi(base.RestView):
     def GET(self, request, *args, **kwargs):
         
         if 'fb_id' not in request.session:
-            data = { 'fb_id': '-1' }
+            data = { 'fb_id': -1 }
         else: data = { 'fb_id': request.session['fb_id'] }
         
         return base.APIResponse(data)
