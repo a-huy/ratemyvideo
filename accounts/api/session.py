4487cd0065b7d9fabc6aa15efa83bbecf7ea8547
Andy Nguyen <andy@mahalo.com>
2012-10-29 10:15:32 -0700

    added DELETE to session api

--- accounts/api/session.py
+++ accounts/api/session.py
@@ -5,9 +5,15 @@ import base.api.base as base
 class SessionApi(base.RestView):
 
     def GET(self, request, *args, **kwargs):
-        
         if 'fb_id' not in request.session:
             data = { 'fb_id': -1 }
         else: data = { 'fb_id': request.session['fb_id'] }
         
         return base.APIResponse(data)
+
+    def DELETE(self, request, *args, **kwargs):
+        if 'fb_id' in request.session:
+            del request.session['fb_id']
+
+        return HttpResponse()
+
