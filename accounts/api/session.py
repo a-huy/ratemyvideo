db075bb98f83a912711fe83395821d8c1a50c4b5
Andy Nguyen <andy@mahalo.com>
2012-10-29 10:56:54 -0700

    changed session GET to return 403

--- accounts/api/session.py
+++ accounts/api/session.py
@@ -6,7 +6,8 @@ class SessionApi(base.RestView):
 
     def GET(self, request, *args, **kwargs):
         if 'fb_id' not in request.session:
-            data = { 'fb_id': -1 }
+            return HttpResponseBadRequest('no session')
+#            data = { 'fb_id': -1 }
         else: data = { 'fb_id': request.session['fb_id'] }
         
         return base.APIResponse(data)
