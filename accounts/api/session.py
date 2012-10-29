800c5ae6836037010e5dab82f9693e03426ff984
Andy Nguyen <andy@mahalo.com>
2012-10-29 10:59:06 -0700

    changed session GET to return -1 on no session

--- accounts/api/session.py
+++ accounts/api/session.py
@@ -6,8 +6,7 @@ class SessionApi(base.RestView):
 
     def GET(self, request, *args, **kwargs):
         if 'fb_id' not in request.session:
-            return HttpResponseBadRequest('no session')
-#            data = { 'fb_id': -1 }
+            data = { 'fb_id': -1 }
         else: data = { 'fb_id': request.session['fb_id'] }
         
         return base.APIResponse(data)
