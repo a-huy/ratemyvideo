19530bbf5dadf91c08e37718a853a752f929fd6a
Andy Nguyen <andy@mahalo.com>
2012-10-31 14:06:03 -0700

    fixed vote api url

--- videos/api/urls.py
+++ videos/api/urls.py
@@ -2,6 +2,9 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('videos.api.rating',
     (r'^rating/$', 'RatingCreateApi'),
+)
+
+urlpatterns += patterns('videos.api.vote',
     (r'^vote/$', 'VoteCreateApi'),
 )
 
