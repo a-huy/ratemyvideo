5052c0433ffc2c5a0c10d712798f91ee13c295d3
Andy Nguyen <andy@mahalo.com>
2012-11-13 12:41:59 -0800

    provided alternative link to chrome xtension

--- base/urls.py
+++ base/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('',
     (r'^accounts/', include('accounts.api.urls')),
     (r'^videos/', include('videos.api.urls')),
+    (r'^get/', 'download_extension', name='download_extension'),
 )
--- base/views.py
+++ base/views.py
@@ -1,1 +1,9 @@
-# Create your views here.
+from django.conf import settings
+from django.shortcuts import render_to_response, render, redirect
+from django.http import HttpResponseRedirect, HttpResponseBadRequest, Http404, \
+    HttpResponse
+from django.template import RequestContext
+
+def download_extension(request):
+    return redirect('content/assets/static/files/ratemyvideo.crx')
+
