f3fd8fe6aee4f9229e31de40a253076e042aab3f
Andy Nguyen <andy@mahalo.com>
2012-11-12 16:39:31 -0800

    added whitelist checking for accounts rating api

--- accounts/api/rating.py
+++ accounts/api/rating.py
@@ -4,20 +4,22 @@ from django.http import HttpResponse, HttpResponseNotFound, HttpResponseBadReque
 import base.api.base as base
 import videos.models as videos_models
 import accounts.models as accounts_models
+from base.contrib import whitelisted
 
 class RatingHistoryApi(base.RestView):
 
     model = videos_models.Rating
-    
+
     def GET(self, request, fb_id, *args, **kwargs):
-        
+        if not whitelisted(fb_id):
+            return HttpResponseForbidden('User has not been authenticated')
         try:
             user = accounts_models.User.objects.get(fb_id=fb_id)
         except accounts_models.User.DoesNotExist:
             return HttpResponseBadRequest('Invalid FB ID')
         ratings = videos_models.Rating.objects.filter(user_id=user.id).order_by('created_date')
         videos = videos_models.Video.objects.filter(id__in=[r.video_id for r in ratings])
-        
+
         ratings_list = []
         for rating in ratings:
             video = filter(lambda x:rating.video_id == x.id, videos)[0]
