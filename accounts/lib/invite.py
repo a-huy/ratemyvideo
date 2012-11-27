e8ab12a243524ab9bd0dcc333c414ba5d5d7663b
Andy Nguyen <andy@mahalo.com>
2012-11-27 11:56:12 -0800

    updated determining location by ip

--- accounts/lib/invite.py
+++ accounts/lib/invite.py
@@ -35,7 +35,7 @@ def get_user_data(args, request):
         if 'access_token' in response else None }
     profile = json.load(urllib.urlopen('https://graph.facebook.com/me?' +
         urllib.urlencode(token_dict)))
-    location = profile['location']['name'] if 'location' in profile else addr_to_us_loc(request.META['REMOTE_ADDR'])
+    location = profile['location']['name'] if 'location' in profile else addr_to_us_loc(request)
     data = {
         'fb_id': profile['id'],
         'real_name': profile['name'],
@@ -88,7 +88,10 @@ def calc_age(birthday):
     if (today.month, today.day) < (int(birthday[0]), int(birthday[1])): age -= 1
     return age
 
-def addr_to_us_loc(addr):
+def addr_to_us_loc(request):
+    if 'HTTP_X_FORWARDED_FOR' in request.META:
+        addr = request.META.get('HTTP_X_FORWARDED_FOR').split(',')[-1].strip()
+    else: addr = request.META.get('REMOTE_ADDR', '127.0.0.1')
     geoip = pygeoip.GeoIP(os.path.join(settings.GEOIP_PATH, 'GeoLiteCity.dat'))
     result = geoip.record_by_addr(addr)
     if not result or result['country_code'] != 'US': return 'Unknown'
