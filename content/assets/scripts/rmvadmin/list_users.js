047bbd95ca51ce640870e7ea37ec90bb0f2860f2
Andy Nguyen <andy@mahalo.com>
2012-12-04 18:56:31 -0800

    fixed export link in list_users

--- content/assets/scripts/rmvadmin/list_users.js
+++ content/assets/scripts/rmvadmin/list_users.js
@@ -1,3 +1,7 @@
 $('#xlsx-export-button').click(function() {
-    window.location = window.location.href + '&export';
+    var ind = window.location.href.indexOf('?');
+    export_str = 'export';
+    if (ind == -1) export_str = '?' + export_str;
+    else export_str = '&' + export_str;
+    window.location = window.location.href + export_str;
 });
