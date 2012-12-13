0281e0d721d58fd2a4af6de26ec1286017d26351
Andy Nguyen <andy@mahalo.com>
2012-12-13 14:56:00 -0800

    removed caching user queues

--- videos/api/rating.py
+++ videos/api/rating.py
@@ -59,7 +59,7 @@ class RatingCreateApi(base.RestView):
         queue_entry.delete() ###
 
         # Invalidate user queue and rating history
-        cache.delete(keys.ACC_USER_QUEUE % account.fb_id)
+#        cache.delete(keys.ACC_USER_QUEUE % account.fb_id)
 #        cache.delete(keys.ACC_USER_HISTORY % account.fb_id)
 
         return base.APIResponse(new_rating.to_json())
