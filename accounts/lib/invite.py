a555448d358eca2661e9918bea7edbeda91d38f8
Andy Nguyen <andy@mahalo.com>
2012-12-14 16:43:36 -0800

    filtered requests where location could not be determined

--- accounts/lib/invite.py
+++ accounts/lib/invite.py
@@ -73,6 +73,7 @@ def account_is_eligible(user):
 #    state = user['location'].split(',')[-1].strip()
 #    if state not in states_whitelist:
 #        return (False, 'User location not in authorized area')
+    if user['location'] == 'Unknown': return (False, 'User location not in authorized area')
     seconds_in_age_limit = 60 * 60 * 24 * 180 # It's actually about 6 months
     limit = int(round(time.time() - seconds_in_age_limit))
     access_token['until'] = limit
