e13e6af2af16d1c5f99b240dadcbd75a4a142105
Andy Nguyen <andy@mahalo.com>
2012-12-15 02:41:15 -0800

    fixed importing heroku settings

--- ratemyvideo/settings/heroku.py
+++ ratemyvideo/settings/heroku.py
@@ -18,7 +18,7 @@ DATABASES = {
     'default': dj_database_url.config()
 }
 
-`# Sendgrid credentials
+# Sendgrid credentials
 EMAIL_HOST_USER = os.environ['SENDGRID_USERNAME']
 EMAIL_HOST_PASSWORD = os.environ['SENDGRID_PASSWORD']
 
