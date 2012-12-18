5c5c3a721daab179f0e652b28168323fa67cc9e4
Andy Nguyen <andy@mahalo.com>
2012-12-18 15:31:04 -0800

    added payout information for user GET api call

--- accounts/api/user.py
+++ accounts/api/user.py
@@ -5,6 +5,8 @@ from django.http import HttpResponse, HttpResponseNotFound, HttpResponseBadReque
 from django.shortcuts import redirect
 from django.core.validators import validate_email
 from django.core.exceptions import ValidationError
+from django.utils.timezone import now
+from django.db.models import Max
 
 import base.api.base as base
 import accounts.models as accounts_models
@@ -27,7 +29,14 @@ class UserUpdateApi(base.RestView):
         if not whitelisted(fb_id):
             return HttpResponseForbidden('User has not been authenticated')
         request.session['fb_id'] = fb_id
-        return base.APIResponse(user.json_safe())
+        po_dates = list(accounts_models.Payout.active.filter(user=user).values_list('created_date', flat=True).order_by('-created_date'))
+        po_dates += [user.created_date]
+        avg_po_time = user_lib.avg_payout_time(po_dates)
+        tslp = user_lib.time_since_last_payout(po_dates)
+        data = user.json_safe()
+        data['avg_po_time'] = '%s|%s' % (avg_po_time.days, tslp.seconds / 3600)
+        data['tslp'] = '%s|%s' % (tslp.days, tslp.seconds / 3600)
+        return base.APIResponse(data)
 
     def PUT(self, request, fb_id, *args, **kwargs):
         if not request.user.is_authenticated(): redirect('/rmvadmin/')
