e3f85e248f1df2190b2479234575ef4513b9fdc9
Andy Nguyen <andy@mahalo.com>
2012-12-19 16:49:24 -0800

    removed the use of the telnet library from memcached dump script

--- scripts/dump_memcached.py
+++ scripts/dump_memcached.py
@@ -1,36 +1,47 @@
+# The majority of this code was taken from http://pastebin.com/JZWZqzMr
+# and modified slightly to fit the project's needs
 import utils.djangoenv
-import telnetlib
 import memcache
+import re
 from django.conf import settings
+from datetime import datetime
+from django.core.cache import cache
+ 
+loc_parts = settings.CACHES['default']['LOCATION'].split(':')
+host = loc_parts[0]
+port = loc_parts[1]
+mc = memcache.Client((':'.join(loc_parts),))
 
-def get_all_memcached_keys(host='127.0.0.1', port=11211):
-    t = telnetlib.Telnet(host, port)
-    t.write('stats items STAT items:0:number 0 END\n')
-    items = t.read_until('END').split('\r\n')
-    keys = set()
-    for item in items:
-        parts = item.split(':')
-        if not len(parts) >= 3:
-            continue
-        slab = parts[1]
-        t.write('stats cachedump {} 200000 ITEM views.decorators.cache.cache_header..cc7d9 [6 b; 1256056128 s] END\n'.format(slab))
-        cachelines = t.read_until('END').split('\r\n')
-        for line in cachelines:
-            parts = line.split(' ')
-            if not len(parts) >= 3:
-                continue
-            keys.add(parts[1])
-    t.close()
-    return keys
+mc_server = mc.servers[0]
+mc_server.connect()
+slabs = []
+mc_server.send_cmd('stats items')
 
-if __name__ == '__main__':
-    loc_parts = settings.CACHES['default']['LOCATION'].split(':')
-    host = loc_parts[0]
-    port = loc_parts[1]
-    keys = get_all_memcached_keys(host, port)
-    cache = memcache.Client((':'.join(loc_parts),))
-    dump = cache.get_multi(keys)
-    for key in dump.keys():
-        print 'KEY:', key
-        print 'VALUE:', dump[key]
-        print '\n'
+while 1:
+        line = mc_server.readline()
+        if not line or line.strip() == 'END': break
+
+        #gets the list of item details
+        item = line.split(' ', 2)
+        #print(item[1])
+
+        slab = item[1].split(':', 2)
+        slab_id = slab[1]
+
+        if slab_id not in slabs:
+                slabs.append(slab_id)
+
+for slab_item in slabs:
+        mc_server.send_cmd('stats cachedump %s 0' % slab_item)
+        while 1:
+                line = mc_server.readline()
+                if not line or line.strip() == 'END': break
+
+                item_details = line.split(' ')
+                value = cache.get(re.search(r'^:\d+:(.*)', str(item_details[1])).groups(1)[0])
+
+                print("\033[93m[%s]\033[0m \n\
+        \033[94mkey: %s\033[0m \n\
+        \033[95mlength: %s\033[0m \n\
+        \033[91mexpires: %s\033[0m \n\
+        value: %s\n"  % (slab_item, item_details[1], item_details[2][1:], datetime.fromtimestamp(int(item_details[4])).strftime("%Y-%m-%d %H:%M:%S"), value))
