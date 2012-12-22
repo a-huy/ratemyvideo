1b54a8883172c6e9bdac8639e65219302d606e29
Andy Nguyen <andy@mahalo.com>
2012-12-21 17:15:37 -0800

    changed missing pp email to only greet the user by first name

--- scripts/email_missing_pp.py
+++ scripts/email_missing_pp.py
@@ -5,4 +5,4 @@ from base.tasks import send_email
 
 first_po_users = am.User.active.filter(balance__gte=10, pp_email='')
 for user in first_po_users:
-    send_email('missing_paypal_email', user.email, [user.real_name], sender='paypal@ratemyvideo.co')
+    send_email('missing_paypal_email', user.email, [user.real_name.split(' ')[0]], sender='paypal@ratemyvideo.co')
