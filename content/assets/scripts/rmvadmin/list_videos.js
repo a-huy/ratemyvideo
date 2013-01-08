00ac09789f4af8d765c20cf6fac4c3d9a08a0404
Andy Nguyen <andy@mahalo.com>
2013-01-07 16:30:19 -0800

    properly moved all static files into the static file manager, set up AWS S3

--- content/assets/scripts/rmvadmin/list_videos.js
+++
@@ -1,8 +0,0 @@
-$('#num-select').change(function() {
-    var num = $('#num-select').val();
-    var loc = window.location.origin + '/rmvadmin/list_videos/?';
-    if (jsonVars['filter']) loc += 'filter=' + jsonVars['filter'] + '&';
-    if (jsonVars['rev'] == 'true') loc += 'rev&';
-    loc += 'num=' + num;
-    window.location = loc;
-});
